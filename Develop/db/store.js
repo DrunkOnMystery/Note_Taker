a0b80d3f5bcca689624a276baab5bc8e2f85dfb2
Ben Brumby <bbrumby00@gmail.com>
2020-10-09 20:05:10 -0400

    created store file

--- Develop/db/store.js
+++ Develop/db/store.js
@@ -1,38 +1,44 @@
 const fs = require("fs");
 const util = require("util");
+// const { response } = require("express");
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
-const { response } = require("express");
+
 
 //Class for your notes
-class Store {
-  constructor(routeName, title, id, text) {
-    this.routeName = routeName;
-    this.title = title;
-    this.id = id;
-    this.text = text;
-  }
 //Defined function that read, write, and delete
+class Store {
 
-  getRouteName() {
-    return this.routeName;
+  readNote() {
+    return readFileAsync("db/db.json", "utf-8");
   }
 
-  getTitle() {
-    return this.title;
-  }
-
-  getID() {
-    return this.id;
-  }
-
-  getText() {
-    return this.text;
+  writeNote(note) {
+    return writeFileAsync("db/db.json", JSON.stringify(note));
   }
 
 
+  addNote(note) {
+    return this.readNote()
+    .then((data) => JSON.parse(data))
+    .then((notes) => [...notes, note])
+    .then((newNotes) => this.writeNote(newNotes));
+  }
 
+  getNotes() {
+    return this.readNote()
+    .then((notes) => JSON.parse(notes));
+  }
 
+  deleteNote(id) {
+    return this.getNotes()
+    .then((notes) => notes.filter((note) => note.id !== id))
+    .then((filteredNotes) => {
+      console.log(filteredNotes);
+      this.writeNote(filteredNotes);
+    })
 
-
+  }
   }
 module.exports = Store;
